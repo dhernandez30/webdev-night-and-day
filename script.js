f1b572f681b8cca0ddb365f198af6bc4d261b240
Hernandez Daniel <20167@columbushs.com>
2019-10-29 09:41:22 -0400

    not done

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#orb').click( function() {        
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
+        }
+        else {
+            $(this).removeClass('moon').addClass('sun');
+        }
+});
+
+
