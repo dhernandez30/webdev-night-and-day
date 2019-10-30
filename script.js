b07b5ced06114e09b826896e19407faa3b1b058a
Daniel Hernandez <dhernandez20167@columbushs.com>
2019-10-29 20:21:49 -0400

    Done

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
         else {
             $(this).removeClass('moon').addClass('sun');
         }
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+    });
 });
-
 
